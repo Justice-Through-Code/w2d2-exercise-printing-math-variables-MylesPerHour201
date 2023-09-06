5f0f5380ce7902f8876eceacf260023e0549bd1d
MylesPerHour201 <beck.mj1993@gmail.com>
2023-09-05 21:03:05 -0400

    Removed lower temp

--- temp_conversion.py
+++ temp_conversion.py
@@ -45,7 +45,7 @@ convert_5_to_fahrenheit()
 
 def hotter_temp():
     print((30.2 * 9/5) + 32)
-    print((85.1 - 32) * 5/9)
+    
 
     # What is hotter, a temperature of 30.2 degrees celsius, or a temperature of 85.1 degrees fahrenheit?
     # Print out the hotter temp: '30.2 degrees celsius' or '85.1 degrees fahrenheit', respectively
